6275b3d2b561de4ec847dd590086fe1da87dfe75
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-05 21:09:42 -0400

    Add output for the PE ratio and intrinsic value. Corrections are needed

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ tesla = yf.Ticker('TSLA')
 # Pull stock prices for 1 month
 prices = tesla.history(period="1mo")
 
-#function to calculate the price per share of a stack
+# Function to calculate the price per share of a stack
 def price_per_share(stock):
   revenue = stock.info['totalRevenue']
   stock_volume = stock.info["sharesOutstanding"]
   return revenue / stock_volume
 
+# Function to calculate price to earnings ratio
+def price_to_earnings(stock):
+  price = stock.info["regularMarketPreviousClose"]
+  eps = stock.info["forwardEps"]
+  return price / eps
 
-print('This is the price per share', price_per_share(tesla))
+
+print('This is tesla\'s pe ratio', price_to_earnings(tesla))
+print('This is tesla\'s intrisic price', price_to_earnings(tesla) * price_per_share(tesla))
