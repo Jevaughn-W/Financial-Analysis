58793b566f2b97a546c1e5659deaa0e105902b1c
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-05 20:37:22 -0400

    Implement basic function to calculate the price per share of Tesla stick

--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 import yfinance as yf
 
 
-
 tesla = yf.Ticker('TSLA')
 
-print (tesla.price)
+# Pull stock prices for 1 month
+prices = tesla.history(period="1mo")
+
+#function to calculate the price per share of a stack
+def price_per_share(stock):
+  revenue = stock.info['totalRevenue']
+  stock_volume = stock.info["sharesOutstanding"]
+  return revenue / stock_volume
+
+
+print('This is the price per share', price_per_share(tesla))
