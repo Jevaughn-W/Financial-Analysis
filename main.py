253d05d6f5f8ec7f9f2ad756a6d23ea587991f1d
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-05 21:58:37 -0400

    Added base code for creating a table in python

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Importing yfinance library
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 
 tesla = yf.Ticker('TSLA')
@@ -20,6 +21,21 @@ def price_to_earnings(stock):
   return price / eps
 
 
-print('This is tesla\'s pe ratio', price_to_earnings(tesla))
-print('This is tesla\'s earnings per share', earnings_per_share(tesla))
-print('This is tesla\'s intrisic price', price_to_earnings(tesla) * earnings_per_share(tesla))
+# print('This is tesla\'s pe ratio', price_to_earnings(tesla))
+# print('This is tesla\'s earnings per share', earnings_per_share(tesla))
+# print('This is tesla\'s intrisic price', price_to_earnings(tesla) * earnings_per_share(tesla))
+
+
+## Sample code of how to construct a table in python
+tesla = yf.Ticker("TSLA")
+data = tesla.history(period="1y")
+
+
+plt.figure(figsize=(12, 6))  # Adjust the figure size as needed
+plt.plot(data.index, data["Close"], label="Stock Price")
+plt.xlabel("Date")
+plt.ylabel("Price (USD)")
+plt.title("Tesla Stock Price Over Time")
+plt.legend()
+plt.grid(True)
+plt.show()
